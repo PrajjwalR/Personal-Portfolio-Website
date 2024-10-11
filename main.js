87cc7fc055a5eb790aa40ad5b9c76c620d4fc87e
Prajjwal Chauhan <prajjwalforwork@gmail.com>
2024-10-11 18:13:59 +0530

    Upate done in 11-10-24

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+window.addEventListener('scroll', function() {
+    const img = document.querySelector('.section_pic-container img');
+    const scrollY = window.scrollY; 
+    const scaleValue = Math.max(0.5, 1 - scrollY / 500);
+
+    img.style.transform = `scale(${scaleValue})`; 
+});
+
